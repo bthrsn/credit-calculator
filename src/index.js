268a15ba965da610228b36e194c6da9f8b42b6dd
bthrsn <mikhail.lhts@gmail.com>
2020-12-09 16:06:38 +0300

    del tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import reportWebVitals from './reportWebVitals';
 import 'normalize.css';
 
 ReactDOM.render(
@@ -10,5 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals();
