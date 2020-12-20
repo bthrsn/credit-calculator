29f4bc3bd3f0a37d716e7966107af7a052350a52
bthrsn <mikhail.lhts@gmail.com>
2020-12-20 10:56:14 +0300

    del zero in empty inputs

--- src/services/displayNumerals.js
+++ src/services/displayNumerals.js
@@ -2,11 +2,11 @@ import numeral from 'numeral';
 
   // Добавления пробелов между разрядами
   const addSpacesToValue = (value) => {
-    // if (value === 0) {
-    //   return '';
-    // } else {
+    if (value === 0 || value === null) {
+      return '';
+    } else {
       return numeral(value).format('0,0');
-    // }
+    }
   }
   // Удаление проблеов между разрядами для подсчета
   const removeSpacesInValue = (value) => {
